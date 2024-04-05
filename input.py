3cca1cd3d9201c8d0819ddc63eaca9305af204d3
Golden1Knight <164238965+Golden1Knight@users.noreply.github.com>
2024-04-05 16:17:22 +0200

    Create input.py

---
+++ input.py
@@ -0,0 +1,31 @@
+global smallInput
+global largeInput
+global bigInput
+global alwaysInput
+alwaysInput = []
+def sI_on():
+  smallInput = [input(), input(), input()]
+def lI_on():
+  largeInput = [input(), input(), input(), input(), input(), input(), input()]
+def bI_on():
+  bigInput = [input(), input(), input(), input(), input(), input(), input(), input(), input(), input(), input(), input(), input(), input(), input()]
+
+def alwaysInputGo(repeatNumber, whether_to_display_notifications=False):
+  if repeatNumber == 00:
+    if whether_to_display_notifications == False:
+      while True:
+        alwaysInput.append(input())
+    else:
+      while True:
+        napis = input()
+        alwaysInput.append(napis)
+        print(f"Written {napis} for [{i}] times")
+  else:
+    if whether_to_display_notifications == False:
+      for i in range(repeatNumber):
+        alwaysInput.append(input())
+    else:
+      for i in range(repeatNumber):
+        napis = input()
+        alwaysInput.append(napis)
+        print(f"Written {napis} for [{i}] times")
