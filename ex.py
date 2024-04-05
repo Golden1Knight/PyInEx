9783a4f8530a3b6f90b5407530a25a698ad9db32
Golden1Knight <164238965+Golden1Knight@users.noreply.github.com>
2024-04-05 16:17:00 +0200

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,5 @@
+import py as nowPy
+import input
+nowPy.defineExtension("__DEF__ [PyInEx]")
+nowPy.downloadExtension(f"$download #{nowPy.lastDefinedExtension} @as 'pie'")
+nowPy.activeExtension("pie.xts")
