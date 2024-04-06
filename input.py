ae35ae038052bff856b41384fc66924a171f641a
Golden1Knight <164238965+Golden1Knight@users.noreply.github.com>
2024-04-06 20:35:45 +0200

    Update input.py to new version: 0.5.2 BETA

--- input.py
+++ input.py
@@ -1,4 +1,12 @@
 import PyInEx.ex as ex
+global baseSettings
+baseSettings = {
+  "textTab": False,
+  "lineNumber": [0,1,2,3,4,5,6,7,8,9],
+  "transparency": [0,0.5,1],
+  "transparencyPercent": ["0%","50%","100%"],
+  "actuallyTransparency": 1
+}
 global smallInput
 global largeInput
 global bigInput
@@ -11,24 +19,20 @@ def lI_on():
 def bI_on():
   bigInput = [input(), input(), input(), input(), input(), input(), input(), input(), input(), input(), input(), input(), input(), input(), input()]
 
-def alwaysInputGo(repeatNumber, whether_to_display_notifications=False):
-  if repeatNumber == "#":
-    if whether_to_display_notifications == False:
-      while True:
+def alwaysInputGo(repeatNumber, whether_to_display_notifications=False, settings=baseSettings):
+  if whether_to_display_notifications is False:
+    if settings.get("textTab") is False:
+      
+
+      for i in range(repeatNumber):
         alwaysInput.append(input())
     else:
-      i = 0
-      while True:
-        i = i + 1
-        napis = input()
-        alwaysInput.append(napis)
-        print(f"Written {napis} for [{i}] times")
+      for i in range(repeatNumber):
+        alwaysInput.append(input("       "))
   else:
-    if whether_to_display_notifications == False:
-      for i in range(repeatNumber):
-        alwaysInput.append(input())
-    else:
-      for i in range(repeatNumber):
-        napis = input()
-        alwaysInput.append(napis)
-        print(f"Written {napis} for [{i}] times")
+    for i in range(repeatNumber):
+      napis = input()
+      alwaysInput.append(napis)
+      print(f"Written {napis} for [{i}] times")
+
+  
