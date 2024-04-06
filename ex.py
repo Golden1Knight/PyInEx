aa88bbca08737628239b6c400218b78cd8595c79
Golden1Knight <164238965+Golden1Knight@users.noreply.github.com>
2024-04-06 20:37:02 +0200

    Update ex.py to 0.5.2 BETA

--- ex.py
+++ ex.py
@@ -2,9 +2,3 @@ global installingExtension
 installingExtension = False
 import PyInEx.py as nowPy
 import PyInEx.input as input
-while True:
-  if installingExtension == False:
-    nowPy.defineExtension("__DEF__ [PyInEx]")
-    nowPy.downloadExtension(f"$download #{nowPy.lastDefinedExtension} @as 'pie'")
-    nowPy.activeExtension("pie.xts")
-    installingExtension = True
