f830cf2afbcc25cd81c20e79db333cae05e91ce0
Golden1Knight <164238965+Golden1Knight@users.noreply.github.com>
2024-04-07 10:27:07 +0200

    Update input.py

--- input.py
+++ input.py
@@ -2,10 +2,8 @@ import PyInEx.ex as ex
 global baseSettings
 baseSettings = {
   "textTab": False,
-  "lineNumber": [0,1,2,3,4,5,6,7,8,9],
-  "transparency": [0,0.5,1],
-  "transparencyPercent": ["0%","50%","100%"],
-  "actuallyTransparency": 1
+  "wheterToRepeat": False,
+  "seeLineNumber": False
 }
 global smallInput
 global largeInput
@@ -20,50 +18,96 @@ def bI_on():
   bigInput = [input(), input(), input(), input(), input(), input(), input(), input(), input(), input(), input(), input(), input(), input(), input()]
 
 def alwaysInputGo(repeatNumber, wheterToRepeat=False, settings=baseSettings):
-  if wheterToRepeat is False:
-    if repeatNumber == "#":
-      if settings.get("textTab") is False:
-        while True:
-          alwaysInput.append(input())
+  if settings.get("seeLineNumber") is False:
+    if wheterToRepeat is False or settings.get("wheterToRepeat") is False:
+      if repeatNumber == "#":
+        if settings.get("textTab") is False:
+          while True:
+            alwaysInput.append(input())
+        else:
+          while True:
+            alwaysInput.append(input("       "))
       else:
-        while True:
-          alwaysInput.append(input("       "))
+        if settings.get("textTab") is False:
+          for i in range(repeatNumber):
+            alwaysInput.append(input())
+        else:
+          for i in range(repeatNumber):
+            alwaysInput.append(input("       "))
     else:
-      if settings.get("textTab") is False:
-        for i in range(repeatNumber):
-          alwaysInput.append(input())
+      if not repeatNumber == "#":
+        if settings.get("textTab") is False:
+          for i in range(repeatNumber):
+            napis = input()
+            print(f"Written '{napis}' in input [{i+1}/{repeatNumber}]")
+        else:
+          for i in range(repeatNumber):
+            napis = input("       ")
+            napis = napis.replace("       ", "")
+            print(f"Written '{napis}' in input {i+1}/{repeatNumber}")
       else:
-        for i in range(repeatNumber):
-          alwaysInput.append(input("       "))
+        if settings.get("textTab") is False:
+          i = 0
+          while True:
+            i = i + 1
+            napis = input()
+            print(f"Written '{napis}' in input [{i}/999999]")
+            if i == 999999:
+              pass
+        else:
+          i = 0
+          while True:
+            i = i + 1
+            napis = input("       ")
+            napis = napis.replace("       ", "")
+            print(f"Written '{napis}' in input [{i}/999999]")
+            if i == 999999:
+              pass
   else:
-    if not repeatNumber == "#":
-      if settings.get("textTab") is False:
-        for i in range(repeatNumber):
-          napis = input()
-          print(f"Written '{napis}' in input [{i+1}/{repeatNumber}]")
-      else:
-        for i in range(repeatNumber):
-          napis = input("       ")
-          napis = napis.replace("       ", "")
-          print(f"Written '{napis}' in input {i+1}/{repeatNumber}")
-    else:
-      if settings.get("textTab") is False:
-        i = 0
-        while True:
-          i = i + 1
-          napis = input()
-          print(f"Written '{napis}' in input [{i}/999999]")
-          if i == 999999:
-            pass
-      else:
-        i = 0
-        while True:
-          i = i + 1
-          napis = input("       ")
-          napis = napis.replace("       ", "")
-          print(f"Written '{napis}' in input [{i}/999999]")
-          if i == 999999:
-            pass
-        
+    #######################################################
+        if wheterToRepeat is False or settings.get("wheterToRepeat") is False:
+          if repeatNumber == "#":
+            if settings.get("textTab") is False:
+              print("YOU MUST SET FUNCTION 'textTab' TO TRUE")
+              pass
+            else:
+              linia = 0
+              while True:
+                linia = linia + 1
+                alwaysInput.append(input(f"{linia}       "))
+          else:
+            if settings.get("textTab") is False:
+              for i in range(repeatNumber):
+                alwaysInput.append(input())
+            else:
+              linia = 0
+              for i in range(repeatNumber):
+                linia = linia + 1
+                alwaysInput.append(input(f"{linia}       "))
+        else:
+          if not repeatNumber == "#":
+            if settings.get("textTab") is False:
+              print("YOU MUST SET FUNCTION 'textTab' TO TRUE")
+            else:
+              linia = 0
+              for i in range(repeatNumber):
+                linia = linia + 1
+                napis = input(f"{linia}       ")
+                napis = napis.replace("       ", "")
+                print(f"Written '{napis}' in input {i+1}/{repeatNumber}")
+          else:
+            if settings.get("textTab") is False:
+              print("YOU MUST SET FUNCTION 'textTab' TO TRUE")
+            else:
+              i = 0
+              linia = 0
+              while True:
+                linia = linia + 1
+                i = i + 1
+                napis = input(f"{linia}       ")
+                napis = napis.replace("       ", "")
+                print(f"Written '{napis}' in input [{i}/999999]")
+                if i == 999999:
+                  pass
 
   
